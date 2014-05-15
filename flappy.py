bca3787f5f72694f53a4654e727cc45043a3f252
Sourabh Verma <sourabh.coder@gmail.com>
2014-05-16 01:05:15 +0530

    Use pixelCollide in checkCrash

--- flappy.py
+++ flappy.py
@@ -365,6 +365,7 @@ def showScore(score):
 
 
 def checkCrash(player, upperPipes, lowerPipes):
+    pi = player['index']
     player['w'] = IMAGES['player'][0].get_width()
     player['h'] = IMAGES['player'][0].get_height()
     if player['y'] + player['h'] + 4 >= BASEY:
@@ -376,13 +377,20 @@ def checkCrash(player, upperPipes, lowerPipes):
         pipeH = IMAGES['pipe'][0].get_height()
 
         for uPipe, lPipe in zip(upperPipes, lowerPipes):
-            uPipe = (uPipe['x'], uPipe['y'], pipeW, pipeH)
-            lPipe = (lPipe['x'], lPipe['y'], pipeW, pipeH)
-            if playerRect.colliderect(uPipe) or playerRect.colliderect(lPipe):
+            # upper and lower pipe rects
+            uPipeRect = pygame.Rect(uPipe['x'], uPipe['y'], pipeW, pipeH)
+            lPipeRect = pygame.Rect(lPipe['x'], lPipe['y'], pipeW, pipeH)
+
+            # player and upper/lower pipe hitmasks
+            pHitMask = HITMASKS['player'][pi]
+            uHitmask = HITMASKS['pipe'][0]
+            lHitmask = HITMASKS['pipe'][1]
+
+            uCollide = pixelCollision(playerRect, uPipeRect, pHitMask, uHitmask)
+            lCollide = pixelCollision(playerRect, lPipeRect, pHitMask, lHitmask)
+
+            if uCollide or lCollide:
                 return [True, False]
-
-    return [False, False]
-
 
     return [False, False]
 
