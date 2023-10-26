0e6cd77a256d073503d751fc4f71ac2809ed3df0
Philip Wanczycki <p.wanczycki@gmail.com>
2023-10-26 11:59:59 -0400

    define state and action spaces

--- src/flappyEnv.py
+++ src/flappyEnv.py
@@ -1,15 +1,32 @@
 import numpy as np
 import pygame
+from flappy import Flappy
 
 import gymnasium as gym
-from gymnasium import spaces
+from gymnasium.spaces import Discrete, Dict, Tuple, Box
 
 
 class FlappyEnv(gym.Env):
-    metadata = {}
+    metadata = {"render_modes": ["human"], "render_fps": 30}
 
     def __init__(self, rendermode=None, size=0):
-        pass
+        self.game = Flappy()
+        self.height = self.game.config.window.height
+        self.width = self.game.config.window.width
+        
+        # obs space is composed of agent height and position of the gap in the next 2 pipes
+        self.observation_space = Dict(
+            {
+                "bird": Discrete(self.height),
+                "pipes_y": Box(0, self.height, shape=(2,), dtype=int),
+                "pipes_x": Box(0, self.width, shape=(2,), dtype=int),
+            }
+        )
+        # 2 actions: 0 -> no action, 1 -> jump
+        self.action_space = Discrete(2)
+
+        assert rendermode is None or rendermode in self.metadata["render_modes"]
+        self.render_mode = rendermode
 
     def _get_obs(self):
         pass
