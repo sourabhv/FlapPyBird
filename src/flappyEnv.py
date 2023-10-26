f3db8410e4f7b6f268386dac5dac102ff9256539
Philip Wanczycki <p.wanczycki@gmail.com>
2023-10-26 10:31:45 -0400

    test commit, create Env

---
+++ src/flappyEnv.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pygame
+
+import gymnasium as gym
+from gymnasium import spaces
+
+
+class FlappyEnv(gym.Env):
+    metadata = {}
+
+    def __init__(self, rendermode=None, size=0):
+        pass
+
+    def _get_obs(self):
+        pass
